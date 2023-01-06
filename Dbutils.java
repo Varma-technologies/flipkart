d0b9a24883d19bb545ac74f2c7ad61b82d1abc2c
Tejavarma1993 <tejavarmacool@gmail.com>
2023-01-06 22:02:14 +0530

    This reverts commit b29e53c25e72696cabd9429c3978ad7858549c5d.

--- Dbutils.java
+++ Dbutils.java
@@ -1,2 +1,1 @@
 Java file
-test git revert
--- test.py
+++ test.py
@@ -1,6 +1,1 @@
-<<<<<<< HEAD
 This file is created for testing git status command
-=======
-Hi, This file is python file
-
->>>>>>> parent of 0a7389f (Revert "testing git revert command")
